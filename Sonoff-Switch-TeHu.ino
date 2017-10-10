0c9d773a410c19dd037bb122a67ae4c8ec9dce5d
Mats Ekberg <matsekberg@gmail.com>
2017-10-10 20:44:34 +0200

    Implemented support submodule

--- Sonoff-Switch-TeHu.ino
+++ Sonoff-Switch-TeHu.ino
@@ -23,158 +23,39 @@
 
 */
 
-#include <ESP8266WiFi.h>
-// must increase max packet size to > 500
-#include <PubSubClient.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-#include "DHT.h"
-
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
-
-#define LONG_PRESS_MS 1000
-#define SHORT_PRESS_MS 100
-#define CONFIG_WIFI_PRESS_MS 5000
-#define CONFIG_TOUCHES_COUNT 3
-#define MQTT_CHECK_MS 15000
-
-//#define F(x) (x)
-
-////// config values
-//define your default values here, if there are different values in config.json, they are overwritten.
-char mqtt_server[40] = "10.0.1.50";
-char mqtt_port[6] = "1883";
-char mqtt_user[24] = "";
-char mqtt_pass[24] = "";
-char unit_id[16] = "tehu0";
-char group_id[16] = "tehugrp0";
-
-// The extra parameters to be configured (can be either global or just in the setup)
-// After connecting, parameter.getValue() will get you the configured value
-// id/name placeholder/prompt default length
-WiFiManagerParameter custom_mqtt_server = NULL;
-WiFiManagerParameter custom_mqtt_port = NULL;
-WiFiManagerParameter custom_mqtt_user = NULL;
-WiFiManagerParameter custom_mqtt_pass = NULL;
-WiFiManagerParameter custom_unit_id = NULL;
-WiFiManagerParameter custom_group_id = NULL;
+// DO EDIT
+#define CONFIG_VERSION "THSW001"
+// END - DO EDIT
 
 
-#define OTA_PASS "UPDATE_PW"
-#define OTA_PORT 8266
+// DO NOT CHANGE
+#include "sensorlibs.h"
+#include "support/wifi-manager.h"
+#include "support/mqtt-support.h"
 
-#define BUTTON_PIN 0
-#define RELAY_PIN 12
-#define LED_PIN 13
+#include "topics.h"
+#include "support/wifi-manager.cpp"
+#include "support/mqtt-support.cpp"
+// END - DO NOT CHANGE
+
+
 #define DHTPIN 14
-
 //#define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 #define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-volatile int desiredRelayState = 0;
-volatile int relayState = 0;
-volatile unsigned long millisSinceChange = 0;
-volatile int noOfConfigTouches = 0;
-
-volatile boolean sendGroupEventTopic = false;
-volatile boolean configWifi = false;
-volatile boolean sendEvent = true;
-boolean sendStatus = true;
-boolean sendPong = false;
-unsigned long uptime = 0;
 
 float humid = NAN;
 float temp = NAN;
 boolean sendSensors = false;
-
-unsigned long lastMQTTCheck = -MQTT_CHECK_MS; //This will force an immediate check on init.
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
 DHT dht(DHTPIN, DHTTYPE);
-
-bool printedWifiToSerial = false;
-
-// these are defined after wifi connect and parameters are set (in setup())
-String eventTopic;       // published when the switch is touched
-String groupEventTopic;  // published when the switch was long touched
-String statusTopic;      // published when the relay changes state wo switch touch
-String sensorTopic;      // publish sensor values
-String pongStatusTopic;  // publish node status topic
-String pongMetaTopic;    // publish node meta topic
-String pingTopic;        // subscribe to nodes ping topic
-String actionTopic;      // subscribed to to change relay status
-String groupActionTopic; // subscribed to to change relay status based on groupid
-
-//
-// Connect to MQTT broker
-// Subscribe to topics, flash LED etc
-//
-void checkMQTTConnection() {
-  Serial.print(F("MQTT conn? "));
-  if (client.connected()) Serial.println(F("OK"));
-  else {
-    if (WiFi.status() == WL_CONNECTED) {
-      //Wifi connected, attempt to connect to server
-      Serial.print(F("new connection: "));
-      if (client.connect(custom_unit_id.getValue(), custom_mqtt_user.getValue(), custom_mqtt_pass.getValue())) {
-        Serial.println(F("connected"));
-        client.subscribe(pingTopic.c_str());
-        client.subscribe(actionTopic.c_str());
-        client.subscribe(groupActionTopic.c_str());
-        client.publish(pongStatusTopic.c_str(), "connected");
-      } else {
-        Serial.print(F("failed, rc="));
-        Serial.println(client.state());
-      }
-    }
-    else {
-      //Wifi isn't connected, so no point in trying now.
-      Serial.println(F(" Not connected to WiFI AP, abandoned connect."));
-    }
-  }
-  //Set the status LED to ON if we are connected to the MQTT server
-  if (client.connected())
-    digitalWrite(LED_PIN, LOW);
-  else
-    digitalWrite(LED_PIN, HIGH);
-}
-
 
 //
 // MQTT message arrived, decode
-// Ok payload: 1/on, 0/off, X/toggle, S/status
 //
-void MQTTcallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallbackHandle(char* topic, byte* payload, unsigned int length) {
   Serial.print(F("MQTT sub: "));
   Serial.println(topic);
-
-  if (!strcmp(topic, actionTopic.c_str()) || !strcmp(topic, groupActionTopic.c_str()))
-  {
-    if ((char)payload[0] == '1' || ! strncasecmp_P((char *)payload, "on", length))
-    {
-      desiredRelayState = 1;
-    }
-    else if ((char)payload[0] == '0' || ! strncasecmp_P((char *)payload, "off", length))
-    {
-      desiredRelayState = 0;
-    }
-    else if ((char)payload[0] == 'X' || ! strncasecmp_P((char *)payload, "toggle", length))
-    {
-      desiredRelayState = !desiredRelayState;
-    }
-    else if ((char)payload[0] == 'S' || ! strncasecmp_P((char *)payload, "status", length))
-    {
-      sendStatus = true;
-    }
-  }
-  if (!strcmp(topic, pingTopic.c_str()))
-  {
-    sendPong = true;
-  }
 }
 
 //
@@ -239,66 +120,17 @@ void buttonChangeCallback() {
 //
 void handleStatusChange() {
 
-  //Relay state is updated via the interrupt *OR* the MQTT callback.
-  if (relayState != desiredRelayState) {
-    Serial.print(F("Chg state to "));
-    Serial.println(desiredRelayState);
-
-    digitalWrite(RELAY_PIN, desiredRelayState);
-    relayState = desiredRelayState;
-    sendStatus = true;
-  }
-
-  if (sendPong)
-  {
-    Serial.print(F("MQTT pub: "));
-    String meta = getDeviceMeta();
-    Serial.print(meta);
-    Serial.print(F(" to "));    
-    Serial.println(pongMetaTopic);
-    client.publish(pongMetaTopic.c_str(), meta.c_str());
-    sendPong = false;
-  }
-
-  // publish event if touched
-  if (sendEvent) {
-    const char* payload = (relayState == 0) ? "off" : "on";
-    Serial.print(F("MQTT pub: "));
-    Serial.print(payload);
-    Serial.print(F(" to "));
-    if (sendGroupEventTopic) {
-      Serial.println(groupEventTopic);
-      client.publish(groupEventTopic.c_str(), payload);
-    } else {
-      Serial.println(eventTopic);
-      client.publish(eventTopic.c_str(), payload);
-    }
-    sendEvent = false;
-  }
-
-  // publish state when requested to do so
-  if (sendStatus) {
-    const char* payload = (relayState == 0) ? "off" : "on";
-    Serial.print(F("MQTT pub: "));
-    Serial.print(payload);
-    Serial.print(F(" to "));
-    Serial.println(statusTopic);
-    client.publish(statusTopic.c_str(), payload);
-    sendStatus = false;
-  }
+  // publish relay state, pong, event and status messages
+  mqttPublish();
 
   if (sendSensors)
   {
-	DynamicJsonBuffer jsonBuffer;
-	JsonObject& json = jsonBuffer.createObject();
-	
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+
     if (!isnan(temp))
     {
-      Serial.print(F("MQTT pub: "));
-      Serial.print(temp);
-      Serial.print(F(" to "));
-      Serial.println(sensorTempTopic);
-	  json["temp"] = String(temp).c_str();
+      json["temp"] = String(temp).c_str();
     }
     else
     {
@@ -306,21 +138,32 @@ void handleStatusChange() {
     }
     if (!isnan(humid))
     {
-      Serial.print(F("MQTT pub: "));
-      Serial.print(humid);
-      Serial.print(F(" to "));
-      Serial.println(sensorHumidTopic);
-  	  json["humid"] = String(humid).c_str();
+      json["humid"] = String(humid).c_str();
     }
     else
     {
       Serial.println(F("No humidity data"));
     }
-	String jsonStr;
-	json.printTo(jsonStr);
+    String jsonStr;
+    json.printTo(jsonStr);
+    Serial.print(F("MQTT pub: "));
+    Serial.print(jsonStr);
+    Serial.print(F(" to "));
+    Serial.println(sensorTopic);
     client.publish(sensorTopic.c_str(), jsonStr.c_str());
     sendSensors = false;
   }
+}
+
+//
+// callback to create custom topics
+//
+void mqttCallbackCreateTopics() {
+  sensorTopic = String(F("sensor/")) + custom_unit_id.getValue() + String(F("/value"));
+
+  // pointer of topics
+  //subscribedTopics[0] = &matrixActionSTopic;
+  noSubscribedTopics = 0;
 }
 
 
@@ -337,28 +180,11 @@ void setup() {
 
   digitalWrite(LED_PIN, HIGH); //LED off.
 
-  initWifiManager(false);
+  // setup wifi
+  wifiSetup(CONFIG_VERSION, false);
 
-  // after wifi and parameters are configured, create publish topics
-  eventTopic = String(F("event/")) + custom_unit_id.getValue() + String(F("/switch"));
-  groupEventTopic = String(F("event/")) + custom_group_id.getValue() + String(F("/switch"));
-  statusTopic = String(F("status/")) + custom_unit_id.getValue() + String(F("/relay"));
-  sensorTopic = String(F("sensor/")) + custom_unit_id.getValue() + String(F("/value"));
-  pongStatusTopic = String(F("pong/")) + custom_unit_id.getValue() + String(F("/status"));
-  pongMetaTopic = String(F("pong/")) + custom_unit_id.getValue() + String(F("/meta"));
-  // and subscribe topic
-  actionTopic = String(F("action/")) + custom_unit_id.getValue() + String(F("/relay"));
-  groupActionTopic = String(F("action/")) + custom_group_id.getValue() + String(F("/relay"));
-  pingTopic = String(F("ping/nodes"));
-
-  client.setServer(custom_mqtt_server.getValue(), atoi(custom_mqtt_port.getValue()));
-  client.setCallback(MQTTcallback);
-
-  // OTA setup
-  ArduinoOTA.setPort(OTA_PORT);
-  ArduinoOTA.setHostname(custom_unit_id.getValue());
-  ArduinoOTA.setPassword(OTA_PASS);
-  ArduinoOTA.begin();
+  // setup mqtt
+  mqttSetup();
 
   dht.begin();
 
@@ -372,19 +198,18 @@ void setup() {
 ////////// LOOP //////////
 //
 void loop() {
-  // If we haven't printed WiFi details to Serial port yet, and WiFi now connected,
-  // do so now. (just the once)
-  if (!printedWifiToSerial && WiFi.status() == WL_CONNECTED) {
-    Serial.println(F("WiFi connected"));
-    Serial.println(F("IP address: "));
-    Serial.println(WiFi.localIP());
-    printedWifiToSerial = true;
-  }
+
+  // handle wifi
+  wifiLoop();
+
+  // handle mqtt
+  mqttLoop();
+
 
   // Check MQTT connection
   if (millis() - lastMQTTCheck >= MQTT_CHECK_MS) {
     uptime += MQTT_CHECK_MS / 1000;
-    checkMQTTConnection();
+    mqttCheckConnection();
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     humid = dht.readHumidity();
     // Read temperature as Celsius (the default)
@@ -393,21 +218,8 @@ void loop() {
     sendSensors = true;
   }
 
-  // Handle any pending MQTT messages
-  client.loop();
-
-  // Handle any pending OTA SW updates
-  ArduinoOTA.handle();
-
   // Handle any state change and MQTT publishing
   handleStatusChange();
-
-  // Handle looong touch to reconfigure all parameters
-  if (configWifi) {
-    espClient.stop();
-    delay(1000);
-    initWifiManager(true);
-  }
 
   delay(50);
 }
--- sensorlibs.h
+++ sensorlibs.h
@@ -2,9 +2,7 @@
 // sensor libs
 
 #include "DHT.h"
-#include <WEMOS_Matrix_LED.h>
+
 
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
-#include <Adafruit_BMP085_U.h>
+
